0e8ff1e70dd7453d0235d5bee85d86a5643b99bb
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-08-26 20:28:00 +0300

    begin agenda

---
+++ RubyShift2013/RubyShift2013AgendaViewController.h
@@ -0,0 +1,13 @@
+//
+//  RubyShift2013AgendaViewController.h
+//  RubyShift2013
+//
+//  Created by Alex on 8/26/13.
+//  Copyright (c) 2013 Alexey Vasyliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RubyShift2013AgendaViewController : UITableViewController
+
+@end
